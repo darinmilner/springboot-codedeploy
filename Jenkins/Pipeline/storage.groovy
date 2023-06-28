dc7726690585993ef1430c41a4bfe732536d85c0
darinmilner <darin.milner@gmail.com>
2023-06-28 21:40:48 +0700

    storage.groovy code refactored

--- Jenkins/Pipeline/storage.groovy
+++ Jenkins/Pipeline/storage.groovy
@@ -1,10 +1,16 @@
 def getAPIEnvFileFromUSEast1Bucket(String awsRegion) {
     try {
         echo "Getting application file from us-east-1 s3 bucket"
-        sh """
-            aws configure set region us-east-1 --profile Default
-            aws s3 cp s3://taskapi-storage-bucket-useast1/application-prod.yaml src/resources/application-prod.yaml --profile Default
-        """
+        withCredentials([usernamePassword(credentialsId: "amazon", usernameVariable: "ACCESSKEY", passwordVariable: "SECRETKEY")]) {
+            sh """
+                aws configure set region us-east-1
+                aws configure set aws_access_key_id $ACCESSKEY 
+                aws configure set aws_secret_access_key $SECRETKEY  
+                aws s3 cp s3://taskapi-storage-bucket-useast1/application-prod.yaml src/resources/application-prod.yaml --profile Default
+                file=\\\$(aws s3 ls  s3://taskapi-storage-bucket-useast-1/envfiles/ --recursive | sort | tail -n 1 | awk '{print \\\$4}') 
+                aws s3 cp src/resources/application-prod.yaml  s3://taskap-storage-bucket-useast1/\\$file 
+            """
+        }
     } catch (Exception err) {
         def errorLib = evaluate readTrusted("Jenkins/Pipeline/errors.groovy")
         echo "Pipeline is exiting $err!"
@@ -38,10 +44,8 @@ def copyEnvFileToRegionalS3Bucket(String bucketName, String awsRegion) {
             sh """
                 aws configure set region ${awsRegion} 
                 aws configure set aws_access_key_id $ACCESSKEY 
-                aws configure set aws_secret_access_key $SECRETKEY 
-                file=\$(aws s3 ls  s3://$bucketName/envfiles/ --recursive | sort | tail -n 1 | awk '{print \$4}') 
-                aws s3 cp src/resources/application-prod.yaml  s3://$bucketName/\$file  
-                # aws s3 cp src/resources/application-prod.yaml s3://${bucketName}/envfiles/application-prod.yaml  --profile Default
+                aws configure set aws_secret_access_key $SECRETKEY  
+                aws s3 cp src/resources/application-prod.yaml s3://${bucketName}/envfiles/application-prod.yaml  --profile Default
             """
         }
     } catch (Exception err) {
